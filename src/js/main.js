33b72d6086799d8fd94b484693c815d8b42f5088
Oleg Vasilev <v.oleg2908@gmail.com>
2024-06-17 20:07:24 +0300

    Добавить модуль effects.js для работы с анимациями

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
 import $ from './lib/lib';
 
-$('button').on('click', function () {
-	$('div').eq(2).toggleClass('active');
-});
-
-$('div').click(function () {
-	console.log($(this).index());
-});
-
-console.log($('.more').eq(0).siblings());
+$('button').fadeIn(1800);
